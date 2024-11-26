307f9ebc4df6141a81d64bd00a846b8aee9ea8b9
Locktum <44526750+Locktum@users.noreply.github.com>
2024-11-26 14:08:35 +0100

    Update page.php

--- themes/mon-theme/page.php
+++ themes/mon-theme/page.php
@@ -6,25 +6,6 @@
             <div class="page-title">
                 <h1><?php the_title(); ?></h1>
             </div>
-            <?php
-            $child_pages = new WP_Query([
-                'post_type' => 'page',
-                'post_parent' => get_the_ID(),
-                'orderby' => 'menu_order',
-                'order' => 'ASC'
-            ]);
-
-            if ($child_pages->have_posts()) :
-            ?>
-                <div class="child-pages-navigation">
-                    <h2>Pages liées</h2>
-                    <ul>
-                        <?php while ($child_pages->have_posts()) : $child_pages->the_post(); ?>
-                            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-                        <?php endwhile; ?>
-                    </ul>
-                </div>
-            <?php endif; wp_reset_postdata(); ?>
             <div class="page-content">
                 <?php the_content(); ?>
             </div>
